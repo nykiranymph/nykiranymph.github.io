ff2b7d385f97c86e2c72c6a08027ccc82a718b33
vijay1996 <vijaybhojraj.cm@gmail.com>
2017-09-22 16:31:55 +0530

    Hosting the first time

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function toggle (){
+	var x = document.getElementsByClassName("topnav");
+
+	if (x[0].style.display == "block"){
+		x[0].style.display = "none";
+	} else {
+		x[0].style.display = "block";
+	}
+	
+}
